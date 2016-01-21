e8f73101f3a687249ec3173635ae6116b32cd86e
Scott Miller <wsmiller324@gmail.com>
2016-01-21 13:21:15 -0800

    couldn't get the form to work

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,53 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+		// $(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+         description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	
+		$("#myelement").hide();
+		$("#myelement").show();
+		$("myelement").toggle();
+
+		$("#myelement").fadeIn();
+		$("#myelement").fadeOut();
+		$("#myelement").fadeToggle();
+
+	}
+    // var containingProject = $(this).closest(".project");
+    // containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
 }
